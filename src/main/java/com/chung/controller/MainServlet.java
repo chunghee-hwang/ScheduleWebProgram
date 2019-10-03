1475e166b25ce4dd21bb526652b9e54873c4bb77
hch0821 <hch0821@gmail.com>
2019-10-03 17:36:21 +0900

    Update MainServlet.java

--- src/main/java/com/chung/controller/MainServlet.java
+++ src/main/java/com/chung/controller/MainServlet.java
@@ -21,20 +21,24 @@ public class MainServlet extends HttpServlet {
 
 	//main.jsp에 필요 정보를 담아서 포워딩해주는 함수
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final String todo = "TODO";
+		final String doing = "DOING";
+		final String done = "DONE";
+		
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("application/json");
 
 		TodoDao dao = TodoDao.getInstance();
 		
 		List<TodoDto> list = dao.getTodos();
-		List<TodoDto> todos = list.stream().filter(dto-> dto.getType().equals("TODO")).collect(Collectors.toList());
-		List<TodoDto> doings = list.stream().filter(dto-> dto.getType().equals("DOING")).collect(Collectors.toList());
-		List<TodoDto> dones = list.stream().filter(dto-> dto.getType().equals("DONE")).collect(Collectors.toList());
+		List<TodoDto> todos = list.stream().filter(dto-> dto.getType().equals(todo)).collect(Collectors.toList());
+		List<TodoDto> doings = list.stream().filter(dto-> dto.getType().equals(doing)).collect(Collectors.toList());
+		List<TodoDto> dones = list.stream().filter(dto-> dto.getType().equals(done)).collect(Collectors.toList());
 		
 		List<List<TodoDto>> plans = new ArrayList<>(Arrays.asList(todos, doings, dones));
 
 		request.setAttribute("plans", plans);
-		request.setAttribute("types", new ArrayList<>(Arrays.asList("TODO", "DOING", "DONE")));
+		request.setAttribute("types", new ArrayList<>(Arrays.asList(todo, doing, done)));
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/main.jsp");
 		dispatcher.forward(request, response);
 	}
